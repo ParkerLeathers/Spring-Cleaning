5c3748d8b34b028c3fe2a1a4c24ed4cf9ab27de1
ParkerLeathers <parkerleathers@gmail.com>
2022-04-30 14:00:43 -0500

    added particles, temp background

--- Assets/Scripts/ParticleHandler.cs
+++ Assets/Scripts/ParticleHandler.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class ParticleHandler : MonoBehaviour
 {
 
-    private ParticleSystem particleSystem;
     
     // Start is called before the first frame update
     void Start()
     {
-        particleSystem = gameObject.GetComponent<ParticleSystem>();
-        particleSystem.Emit(100);
-        particleSystem.Pause();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
-            particleSystem.Play();
-        
+
     }
 }
---
+++ Assets/Scripts/ParticleSystemCollision.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemCollision : MonoBehaviour
+{
+
+    private ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponentInParent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ps.Stop();
+        Destroy(this);
+    }
+}
