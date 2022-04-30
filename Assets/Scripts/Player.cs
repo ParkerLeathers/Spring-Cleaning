325e0ca6270a34e8310843cd5c77d73012d4b0cf
ParkerLeathers <parkerleathers@gmail.com>
2022-04-30 12:42:49 -0500

    added rotation code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float speed = 20.0f;
+    private float speed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +16,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print(rb.velocity);
+        
         Vector2 vec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         if (vec.magnitude > 1)
             vec.Normalize();
-
         if (vec.x != 0 || vec.y != 0)
-            Quaternion.RotateTowards(transform.rotation, new Quaternion(vec.x,vec.y,0,0), Time.deltaTime);
-         rb.velocity = vec * Time.deltaTime * speed;
+        {
+            float deltaAng = Vector2.SignedAngle(new Vector2(Mathf.Cos(rb.rotation * Mathf.Deg2Rad), Mathf.Sin(rb.rotation * Mathf.Deg2Rad)), new Vector2(vec.x, vec.y));
+            print(deltaAng);
+            rb.MoveRotation(90);
+            float move = 1;
+
+            if (Mathf.Abs(deltaAng) < move) {
+                move = Mathf.Abs(deltaAng);
+            }
+            if (deltaAng < 0) {
+                move *= -1;
+            }
+
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, rb.rotation + move));
+        }
+        rb.velocity = vec * speed;
+
     }
 }
